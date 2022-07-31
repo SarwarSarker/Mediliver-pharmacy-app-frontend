928850e07ce69ef0f4a95f37537da57de29088bb
SarwarSarker <sarwarsarker00@gmail.com>
2022-07-31 22:13:43 +0600

    login page ui updated

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -10,7 +10,7 @@ const Register = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { user, isLoading, isError, isSuccess, message } = useSelector(
+  const { user, isError, isSuccess, message } = useSelector(
     (state) => state.auth
   );
 
