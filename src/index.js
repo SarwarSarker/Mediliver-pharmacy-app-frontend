50a64c60ba6d4a09fa84601fcc90ee5dccc6c7e4
SarwarSarker <sarwarsarker00@gmail.com>
2022-07-31 23:04:34 +0600

    ui updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { store } from "./app/store";
 import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./app/store";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
